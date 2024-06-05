ff90f1b5ce32709902d800e2b3b07c8788fa84e2
KG\CRUNDW01 <wilson.crunden@kingfisher.com>
2024-06-05 13:46:49 +0100

    chore: migrate docs to GitHub wiki
    I've also changed the shared package code stub to match the new shared package name.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,7 +1,7 @@
 // This is a stub for a package containing
 // libraries and types used by both the backend
 // and frontend, and can be imported with
-// import { ... } from "@wintrchess/shared"
+// import { ... } from "wintrchess"
 
 // An example module in the shared package with an enum
 // defining sources from which a Stockfish evaluation came from
