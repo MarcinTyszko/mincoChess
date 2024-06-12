22d6b6053e6f99db9a820f3eed2691bd641bb922
KG\CRUNDW01 <wilson.crunden@kingfisher.com>
2024-06-12 11:50:53 +0100

    feat(fe): implement 404 page

--- client/src/pages/Unfound/index.tsx
+++ client/src/pages/Unfound/index.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Button from "@components/common/Button";
+import ButtonColour from "@constants/ButtonColour";
+
 import * as styles from "./Unfound.module.css";
 
 function Unfound() {
     return <div className={styles.wrapper}>
-        <h1>404</h1>
+        <h1 className={styles.errorCode}>404</h1>
         <span>Looks like you're lost.</span>
 
-        <img src={require("@assets/img/unfound.gif")} />
+        <img
+            className={styles.image}
+            src={require("@assets/img/unfoundgame.gif")}
+            width={"45%"}
+        />
 
-        <Link to="/">Return Home</Link>
+        <Link to="/">
+            <Button 
+                colour={ButtonColour.BLUE}
+                icon={require("@assets/img/back.svg")}
+                options={{
+                    iconSize: "30px",
+                    padding: "5px 10px"
+                }}
+            >
+                Return Home
+            </Button>
+        </Link>
     </div>;
 }
 
