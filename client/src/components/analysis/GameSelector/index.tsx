30ee8812044ccdb4883a62cbb560ae5ece8d325d
WintrCat <wintrcat.wilson@gmail.com>
2024-07-03 04:37:03 +0100

    feat(fe): add game source and input game saving with cookies
    I have implemented cookies to save the game source and input game across sessions.

--- client/src/components/analysis/GameSelector/index.tsx
+++ client/src/components/analysis/GameSelector/index.tsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Cookies } from "react-cookie";
 
+import Cookie from "@constants/Cookie";
 import GameSource from "@constants/GameSource";
 import Button from "@components/common/Button";
 import ButtonColour from "@constants/ButtonColour";
@@ -10,26 +12,56 @@ import * as styles from "./GameSelector.module.css";
 
 function GameSelector() {
     const { t } = useTranslation();
+    const cookies = new Cookies();
 
     const [ source, setSource ] = useState<GameSource>(GameSource.PGN);
     const [ searchMenuOpen, setSearchMenuOpen ] = useState(false);
 
-    const inputGameRef = useRef<string>("");
+    const inputGameRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        const savedSource = Object.values(GameSource).find(source => {
+            return source.key == cookies.get(Cookie.LAST_USED_GAME_SOURCE);
+        });
+
+        if (savedSource) setSource(savedSource);
+    }, []);
 
     function handleGameSourceChange(event: React.ChangeEvent<HTMLSelectElement>) {
         const selectedSource = Object.values(GameSource)
             .find(source => source.key == event.target.value);
-
         if (!selectedSource) return;
+
         setSource(selectedSource);
+        cookies.set(Cookie.LAST_USED_GAME_SOURCE, selectedSource.key);
+
+        if (!inputGameRef.current) return;
+        inputGameRef.current.value = cookies.get(
+            Cookie.LAST_CHESS_WEBSITE_USERNAME
+        )?.[selectedSource.key] || "";
     }
 
     function handleGameInputChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        inputGameRef.current = event.target.value;
+        if (
+            source.key == GameSource.PGN.key 
+            || source.key == GameSource.FEN.key
+        ) return;
+
+        const savedUsernames = cookies.get(Cookie.LAST_CHESS_WEBSITE_USERNAME) || {};
+        if (typeof savedUsernames != "object") return;
+
+        cookies.set(
+            Cookie.LAST_CHESS_WEBSITE_USERNAME,
+            {
+                ...savedUsernames,
+                [source.key]: event.target.value
+            }
+        );
     }
 
     function openGameSearchMenu() {
-        if (inputGameRef.current.length == 0) return;
+        if (!inputGameRef.current) return;
+        if (inputGameRef.current.value.length == 0) return;
         setSearchMenuOpen(true);
     }
 
@@ -42,6 +74,7 @@ function GameSelector() {
             <select 
                 className={styles.gameSourceSelector}
                 onChange={handleGameSourceChange}
+                value={source.key}
             >
                 {
                     Object.values(GameSource)
@@ -54,6 +87,7 @@ function GameSelector() {
 
         <textarea
             className={styles.inputGame}
+            ref={inputGameRef}
             placeholder={
                 t(`pages.analysis.gameSelector.sourcePlaceholders.${source.key}`)
             }
@@ -62,6 +96,10 @@ function GameSelector() {
                 borderRadius: source.requiresSearch ? undefined : "0 0 10px 10px"
             }}
             onChange={handleGameInputChange}
+            defaultValue={
+                cookies.get(Cookie.LAST_CHESS_WEBSITE_USERNAME)?.[source.key]
+                || ""
+            }
         ></textarea>
 
         {
@@ -82,7 +120,7 @@ function GameSelector() {
         {
             searchMenuOpen
             && <GameSearchMenu
-                username={inputGameRef.current}
+                username={inputGameRef.current?.value || ""}
                 gameSource={source}
                 setOpen={setSearchMenuOpen}
             />
---
+++ client/src/constants/Cookie.ts
@@ -0,0 +1,6 @@
+enum Cookie {
+    LAST_USED_GAME_SOURCE = "wintrchess_last_used_game_source",
+    LAST_CHESS_WEBSITE_USERNAME = "wintrchess_last_chess_website_username"
+}
+
+export default Cookie;
