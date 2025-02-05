063b806dacf452db2cd7f85d81145fd901231955
WintrCat <wintrcat.wilson@gmail.com>
2025-02-05 18:01:03 +0000

    style(fe): add highlight styles for selected state tree node

--- client/src/components/analysis/StateTreeEditor/components/Move/index.tsx
+++ client/src/components/analysis/StateTreeEditor/components/Move/index.tsx
@@ -3,12 +3,18 @@ import React, { useContext } from "react";
 import MoveClickEventContext from "../../MoveClickEventContext";
 import MoveProps from "./MoveProps";
 import * as styles from "./Move.module.css";
+import useAnalysisGameStore from "@stores/AnalysisGameStore";
 
 function Move({ stateTreeNode, children }: MoveProps) {
     const onMoveClick = useContext(MoveClickEventContext);
 
+    const { currentStateTreeNode } = useAnalysisGameStore();
+
     return <span
-        className={styles.wrapper}
+        className={
+            styles.wrapper
+            + ` ${currentStateTreeNode == stateTreeNode && styles.current}`
+        }
         onClick={() => {
             if (stateTreeNode) onMoveClick?.(stateTreeNode);
         }}
