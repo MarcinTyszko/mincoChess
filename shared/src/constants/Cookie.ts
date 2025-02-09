37baa08fe960467f97f6351aeccdf2d6cef4c1c6
WintrCat <wintrcat.wilson@gmail.com>
2025-02-09 02:13:05 +0000

    chore(sh): remove unused cookie values for game selector
    oops forgot

--- shared/src/constants/Cookie.ts
+++ shared/src/constants/Cookie.ts
@@ -1,7 +1,5 @@
 enum Cookie {
     ADMIN_SESSION_TOKEN = "wintrchess_admin_session_token",
-    LAST_GAME_SELECTOR_SOURCE = "wintrchess_last_used_game_source",
-    LAST_GAME_SELECTOR_INPUTS = "wintrchess_last_game_selector_inputs",
     PREFERRED_LANGUAGE = "wintrchess_preferred_language"
 }
 
