5315fcdf47f2cbc75cd59986fcb6db0f9ed506d9
The Brigade <wintrcat.wilson@gmail.com>
2025-04-04 18:27:59 +0100

    style(fe): add font for important information, finish colouring

--- client/src/components/layout/NavigationBar/index.tsx
+++ client/src/components/layout/NavigationBar/index.tsx
@@ -50,7 +50,8 @@ function NavigationBar() {
                 <Button
                     style={{
                         background: "linear-gradient(-225deg,"
-                            + "#22E1FF 0%, #1D8FE1 48%, #625EB1 100%)"
+                            + "#22E1FF 0%, #1D8FE1 48%, #625EB1 100%)",
+                        fontFamily: "Nunito"
                     }}
                     icon={require("@assets/img/kofi.svg")}
                 >
