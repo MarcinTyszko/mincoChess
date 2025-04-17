c5dcba86e3565b726718bb62d217903706a59ed1
The Brigade <wintrcat.wilson@gmail.com>
2025-04-17 04:49:33 +0100

    fix(sh): count defenders dynamically based on available captures thereof

--- shared/src/lib/reporter/utils/defenders.ts
+++ shared/src/lib/reporter/utils/defenders.ts
@@ -1,4 +1,5 @@
-import { Chess } from "chess.js";
+import { Chess, QUEEN } from "chess.js";
+import { minBy } from "lodash";
 
 import BoardPiece from "./types/BoardPiece";
 import { flipAdaptedPieceColour } from "@lib/moveNotation";
@@ -11,16 +12,48 @@ export function getDefenders(
 ) {
     const defenderBoard = new Chess(board.fen());
 
-    const flippedPiece = {
-        type: piece.type,
-        color: flipAdaptedPieceColour(piece.color)
-    };
+    const attackers = getAttackers(defenderBoard, piece, false);
 
-    defenderBoard.put(flippedPiece, piece.square);
+    // Where there are attackers, simulate taking the piece with each attacker
+    // and record the minima of recaptures
+    const smallestRecapturerSet = minBy(
+        attackers.map(attacker => {
+            const captureBoard = new Chess(defenderBoard.fen());
 
-    return getAttackers(
-        defenderBoard,
-        { ...flippedPiece, square: piece.square },
-        transitive
+            try {
+                captureBoard.move({
+                    from: attacker.square,
+                    to: piece.square,
+                    promotion: QUEEN
+                });
+            } catch {
+                return null;
+            }
+
+            return getAttackers(
+                captureBoard,
+                { ...attacker, square: piece.square },
+                transitive
+            );
+        }).filter(
+            recapturers => !!recapturers
+        ),
+        recapturers => recapturers.length
     );
+
+    // Where there are no attackers, flip the colour of the piece and count
+    // the attackers of the flipped piece
+    if (!smallestRecapturerSet) {
+        const flippedPiece: BoardPiece = {
+            type: piece.type,
+            color: flipAdaptedPieceColour(piece.color),
+            square: piece.square
+        };
+
+        defenderBoard.put(flippedPiece, piece.square);
+        
+        return getAttackers(defenderBoard, flippedPiece, transitive);
+    }
+
+    return smallestRecapturerSet;
 }
